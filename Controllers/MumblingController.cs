240e4d9b85f5e2a03cb3d1df8514285d4baa17cc
Katherine Trammell <ktrammell.com>
2021-01-29 13:41:34 -0500

    MumblingController accepts a string and returns result to match https://www.codewars.com/kata/mumbling

--- Controllers/MumblingController.cs
+++ Controllers/MumblingController.cs
@@ -11,5 +11,27 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class MumblingController : ControllerBase
     {
+        [HttpGet]
+        public string ConvertMumbling(string letters)
+        {
+            string finalString = "";
+
+            for (var index = 0; index < letters.Length; index++)
+            {
+
+                var currentLetter = letters[index];
+                var letterUpper = char.ToUpper(currentLetter);
+                var letterLower = char.ToLower(currentLetter);
+                string lowerLettersRepeated = new String(letterLower, index);
+                var newString = letterUpper + lowerLettersRepeated;
+
+                finalString = finalString + newString;
+                if (index != letters.Length - 1)
+                {
+                    finalString = finalString + "-";
+                }
+            }
+            return finalString;
+        }
     }
 }
